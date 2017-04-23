a1c723030441b877c50e92bf32826c58002b0baf
phirada Thaicaroen <phirada Thaicaroen>
2017-04-23 17:18:25 +0700

    add index.php

--- index.php
+++ index.php
@@ -19,8 +19,12 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $text
+				'text' => "I don' understand"
 			];
+			if ($text == "hi" || $text == "hi" || $text == "hello"){
+			'type' => 'text',
+				'text' => "hello :)"
+			}
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
